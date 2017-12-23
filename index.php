8870043a5475c8f6afc324d18eaaf56fd2c88ea4
Austin Truchan <austintruchan@gmail.com>
2017-12-22 23:41:15 -0500

    Many updates including bg blobs and action items!

--- index.php
+++ index.php
@@ -7,27 +7,38 @@
         <link rel="apple-touch-icon" href="style/img/icon.png">
         <link rel="stylesheet" href="style/css/normalize.css">
         <link rel="stylesheet" href="style/css/main.css">
+        <link rel="stylesheet" href="style/css/fonts.css">
         <link rel="stylesheet" href="style/css/global.css">
+        <link rel="stylesheet" href="style/css/fader.css">
+        <link rel="stylesheet" href="style/css/bg.css">
         <link rel="stylesheet" href="style/css/navigation.css">
+        <link rel="stylesheet" href="style/css/home.css">
     </head>
 	<body>
+		
 		<div class="frame">
+			
 			<nav>
 				<ul class="nav">
 					<li><a href="index.php"><img class="logo" src="style/imgs/logo.svg" alt="Austin Truchan Logo"/></a></li>
 					<li><a href="pages/work.php" class="navlink">Work</a></li>
-					<li><a href="pages/about.php" class="navlink">About</a></li>
-				</ul>
-			</nav>
-			<h1>I’m Austin Truchan, a Digital Designer based in Michigan.<br>I specialize in UI/UX, Interaction & Web Design.<br>I am the Director of Design at <a href="pages/work.php" class="link">Matrix.</a></h1>
-		</div>
+					<li><a href="pages/about.php" class="navlink">About</a></li></ul></nav>
+			
+			<div class="name"><img src="style/imgs/name.svg" alt="Austin Truchan"/></div>
+			
+			<h1 class="intro">I’m Austin Truchan, a Digital Designer based in Michigan.<br>I specialize in UI/UX, Interaction & Web Design.<br>I am the Director of Design at <a href="pages/work.php" class="link">Matrix.</a></h1>
+			
+			<div class="intro-action"><a href="contact.php" class="link-subActive">Let's Work Together</a><a href="pages/about.php" class="link-subActive">Learn More</a></div>
+			
+			
+			<div class="bg-top"><img src="style/imgs/bg-top.svg"/>
 		
 		
-        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="style/js/vendor/modernizr-3.5.0.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+        <script>window.jQuery || document.write('<script src="style/js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+        <script src="style/js/plugins.js"></script>
+        <script src="style/js/main.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
         <script>
