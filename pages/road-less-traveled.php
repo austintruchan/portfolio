99411ae456b7f92f2f37e4ffd91929f9e625c4e5
Austin Truchan <austintruchan@gmail.com>
2018-02-14 23:02:40 -0500

    Minor Fixes and additions to Road Less Traveled

--- pages/road-less-traveled.php
+++ pages/road-less-traveled.php
@@ -25,20 +25,20 @@
 			<div class="project-cover road-less-traveled">
 				<div class="project-cover-content">
 					<h1>The Road Less Traveled</h1>
-					<h2>Expiremental Web Design for an Adventure Gear Retailer.</h2></div></div>
+					<h2>Expiremental Web Design for an Adventure Gear Shop</h2></div></div>
 			
 			<div class="project-content">
 				
-				<p>This is a fun little concept I’ve been working on lately called “The Road Less Traveled” an adventure gear retailer who’s mission is simply getting everyone deep into uncharted wilderness, in attempts to find the purest connection between mankind and nature. Their brand is sophisticated, yet rugged, defining the quality of their products, and the rough terrain that their products allow them to travel travel through. Theres a lot of details here, so feel free to select the image to view it in full.
+				<p>This is a fun little concept I’ve been working on lately called “The Road Less Traveled” an adventure gear shop who’s mission is simply getting everyone deep into uncharted wilderness, in attempts to find the purest connection between mankind and nature. Their brand is sophisticated, yet rugged, defining the quality of their products, and the rough terrain that their products allow them to travel through. Theres a lot of details here, so feel free to select the image to view it in full.
 </p>
 				
 				<a href="#img1"><img src="../style/imgs/projects/road-less-traveled/1.jpg"/></a>
 				<a onclick="goBack()" class="lightbox" id="img1"><img src="../style/imgs/projects/road-less-traveled/1.jpg"></a>
 				
-				<a href="#img1"><img src="../style/imgs/projects/road-less-traveled/2.jpg"/></a>
-				<a onclick="goBack()" class="lightbox" id="img1"><img src="../style/imgs/projects/road-less-traveled/2.jpg"></a>
+				<a href="#img2"><img src="../style/imgs/projects/road-less-traveled/2.jpg"/></a>
+				<a onclick="goBack()" class="lightbox" id="img2"><img src="../style/imgs/projects/road-less-traveled/2.jpg"></a>
 				
-				<p>Check it out, and give it a like on <a href="https://dribbble.com/austintruchan" target="_blank" class="link-inline">Dribbble!</a></p>
+				<p>Check it out, and give it a like on <a href="https://dribbble.com/austintruchan" target="_blank" class="link-inline">Dribbble!</a> (Actually Coming Soon).</p>
 		
 		</div></div>
 
