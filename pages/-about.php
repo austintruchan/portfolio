d3475a55347cc437753cf38554f9aff6673410ee
Austin Truchan <austintruchan@gmail.com>
2018-02-17 22:05:22 -0500

    Updated Bio!

--- pages/-about.php
+++ pages/-about.php
@@ -25,7 +25,26 @@
 					
 					<h2>hey there.</h2>
 				
-			<p class="about">I'm Austin. I’m a Digital Designer currently residing in East Lansing, Michigan. I specialize in UI/UX, Interaction & Web Design, but dabble in everything from frontend development, to logo/icon design, to experimental graphic design. I am also rather obsessed with dystopic scifi genres, and the clash of geometric and organic forms.<br><br> If you're interested in working together, have feedback for this site or any of my works, or just want to say "hey," please feel free to get in touch!</p>
+			<p class="about">I'm Austin. I’m a Digital Designer currently residing in East Lansing, Michigan. I specialize in UI/UX, Interaction & Web Design, but dabble in everything from frontend development, to logo/icon design, to experimental graphic design. I am also rather obsessed with dystopic scifi genres, and the clash of geometric and organic forms. I currently work for a digital humanities web design studio, Matrix, as their Head of Design, and also work remotely as the Design Lead for DishRoots, a startup focused on designing an authentic dish discovery platform.</p>
+			
+			<div class="link-frame">
+			<a href="../style/imgs/AustinTruchan-Resume.pdf" class="link-action" target="_blank">View Resume</a></div>
+			
+			<br><br><br><br>
+			<h2>my story.</h2>
+			<p class="about">It all started in middle school when my dad handed me his bootlegged copy of photoshop. I instantly fell in love with the program and the ability to alter imagery, and arrange digital shapes  and typography to make my own unique compositions. I soon found myself designing myspace avatars, facebook cover photos and album artwork for my all of my friends, and all of their many bands. I was in love with the craft of digital design. 
+<br><br>
+I went to Michigan State University and studied Interactive Web Design with a specialization in Design. Here I become apart of many groups that focused on digital media. I redesigned and developed the website for two different film clubs, I worked with a street team that helped out non-profits by rebranding their identities for the greater good, and worked as the lead designer for the Michigan State Hackathon, Spartahack. I also worked for Matrix, a digital humanities web design studio as a student designer and developer. After graduation, they just had to hire me as a full time employee, where I now work as their Head of Design and Frontend Development. 
+<br><br>
+Outside of Matrix, I continue to design, design & do some more design. I also work remotely for a startup in California, DishRoots, where I work on iOS and Web design. I also find time to work on several of my own side projects, and daily exercises that keep me experimenting and tinkering within the realms of digital design.</p>
+
+		<br><br><br>
+			<h2>likes.</h2>
+			<p class="about">When i'm not designing, i'm probably walking and hiking all of the local nature trails, making myself a hot pour-over coffee, or reading and learning about something new. I've recently fallen in love with zen-ing out, learning how to meditate and do yoga, and becoming a minimalist. Oh! and I finally got a DSLR, so it's been fun experimenting and learning how that  thing works. They say i'm soon to be the next, big instagram star.</p>
+
+		<br><br><br>
+			<h2>get in touch!</h2>
+			<p class="about">If you're interested in working together, have feedback for this site or any of my works, or just want to say "hey," please feel free to get in touch!</p>
 			
 			<div class="link-frame">
 			<a href="mailto:austintruchan@me.com?&subject=Hello.&body=So.... whats up?" class="link-action">austintruchan@me.com</a></div>
