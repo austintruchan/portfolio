309640a6d9e6182b264f5c9a5294791cf64dea41
Austin Truchan <austintruchan@gmail.com>
2018-06-06 23:24:29 -0400

    Updated Email Text, and Addition of Info Page Image

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,6 @@
 <footer class="footer">
 	<ul>
-  	<li><a href="mailto:austintruchan@me.com?&subject=Hello!&body=Hello! I hope you're enjoying your stay at austintruchan.net. If you are, or if you aren't, do let me know why! I would love to hear your feedback on the UI or UX of this site, or any one of my portfolio pieces. Eg: If you think its weird that this email comes stocked with this message, let me know! Or If you have any questions you're wanting to ask me, ask away! Looking forward to hearing from you. Cheers!" class="link-action">say hello!</a></li>
-  	<li><a href="https://github.com/austintruchan/portfolio/issues" class="link-action" target="_blank">found a bug?</a></li>
+  	<li><a href="mailto:austintruchan@me.com?&subject=Hello!&body=So... What's up?" class="link-action">say hello!</a></li>
   	<li><a href="https://twitter.com/austintruchan" target="_blank" title="Twitter" class="link-list">tw</a></li>
   	<li><a href="https://medium.com/@austin.truchan" target="_blank" title="Medium" class="link-list">md</a></li>
   	<li><a href="https://dribbble.com/austintruchan" target="_blank" title="Dribbble" class="link-list">db</a></li>
--- info.php
+++ info.php
@@ -23,30 +23,32 @@
 					<li><a href="work" class="link-list">work</a></li>
 					<li><a href="info" class="link-list active">info</a></li></ul></nav>
 					
-					<h2>hey there.</h2>
+					<h2>welcome.</h2>
+					
+					<div class="me"><div class="img-wrap"><div class="overlay"></div><img src="style/imgs/me.jpg"/></div></div>
 				
-			<p class="about">I'm Austin. I’m a Digital Designer currently residing in East Lansing, Michigan. I specialize in UI/UX, Interaction & Web Design, but dabble in everything from frontend development, to logo/icon design, to experimental graphic design. I am also rather obsessed with dystopic scifi genres, and the clash of geometric and organic forms. I currently work for a digital humanities web design studio, Matrix, as their Head of Design, and also work remotely as the Design Lead for DishRoots, a startup focused on designing an authentic dish discovery platform.
+					<p class="about">I'm Austin. I’m a Digital Designer currently residing in East Lansing, Michigan. I specialize in UI/UX, Interaction & Web Design, but dabble in everything from frontend development, to logo/icon design, to experimental graphic design. I am also rather obsessed with dystopic scifi genres, and the clash of geometric and organic forms. I currently work for a digital humanities web design studio, Matrix, as their Head of Design, and also work remotely as the Design Lead for DishRoots, a startup focused on designing an authentic dish discovery platform.
 			
-			<br><br>If you're interested in working together, have feedback for this site or any of my works, or just want to say "hey," please feel free to get in touch!</p>
+					<br><br>If you're interested in working together, have feedback for this site or any of my works, or just want to say "hey," please feel free to get in touch!</p>
 			
-			<div class="link-frame">
-			<a href="mailto:austintruchan@me.com?&subject=Hello!&body=So.... whats up?" class="link-action">austintruchan@me.com</a></div>
+					<div class="link-frame">
+					<a href="mailto:austintruchan@me.com?&subject=Hello!&body=So.... What's up?" class="link-action">austintruchan@me.com</a></div>
 			
-			<div class="link-frame">
-			<a href="AustinTruchan-Resume.pdf" class="link-action" target="_blank">View Resume</a></div>
+					<div class="link-frame">
+					<a href="AustinTruchan-Resume.pdf" class="link-action" target="_blank">View Resume</a></div>
 			
 			
-			<br><br><br><br>
-			<h2>my story.</h2>
-			<p class="about">It all started in middle school when my dad handed me his bootlegged copy of photoshop. I instantly fell in love with the program and the ability to alter imagery, and arrange digital shapes  and typography to make my own unique compositions. I soon found myself designing myspace avatars, facebook cover photos and album artwork for my all of my friends, and all of their many bands. I was in love with the craft of digital design. 
-<br><br>
-<span>I went to Michigan State University and studied Interactive Web Design with a specialization in Design</span>. Here I fell even deeper in love with digital design, and become apart of many groups that focused on digital media. I redesigned and developed the website for two different film clubs, I worked with a street team that helped out non-profits by rebranding their identities for the greater good, I worked with two startups that were developing iOS apps, and worked as the lead designer for the Michigan State Hackathon, Spartahack. I also worked for Matrix, a digital humanities web design studio as a student designer and developer. After graduation, they just had to hire me as a full time employee, where I now work as their Head of Design. 
-<br><br>
+					<br><br><br><br>
+					<h2>my story.</h2>
+					<p class="about">It all started in middle school when my dad handed me his bootlegged copy of photoshop. I instantly fell in love with the program and the ability to alter imagery, and arrange digital shapes  and typography to make my own unique compositions. I soon found myself designing myspace avatars, facebook cover photos and album artwork for my all of my friends, and all of their many bands. I was in love with the craft of digital design. 
+					<br><br>
+					<span>I went to Michigan State University and studied Interactive Web Design with a specialization in Design</span>. Here I fell even deeper in love with digital design, and become apart of many groups that focused on digital media. I redesigned and developed the website for two different film clubs, I worked with a street team that helped out non-profits by rebranding their identities for the greater good, I worked with two startups that were developing iOS apps, and worked as the lead designer for the Michigan State Hackathon, Spartahack. I also worked for Matrix, a digital humanities web design studio as a student designer and developer. After graduation, they just had to hire me as a full time employee, where I now work as their Head of Design. 
+					<br><br>
 Outside of Matrix, I continue to design, design & do more design. I work remotely for a startup in California, DishRoots, where I work on iOS and Web design. I also find time to work on several of my own side projects, and daily exercises that keep me experimenting and tinkering within the realms of digital design.</p>
 
-		<br><br><br>
-			<h2>likes.</h2>
-			<p class="about">When i'm not designing, i'm probably walking and hiking all of the local nature trails, making myself a hot pour-over coffee, or reading and learning about something new. I've recently fallen in love with zen-ing out, learning how to meditate and do yoga, becoming a minimalist, and how to properly shoot a DSLR. </p>
+					<br><br><br>
+					<h2>likes.</h2>
+					<p class="about">When i'm not designing, i'm probably walking and hiking all of the local nature trails, making myself a hot pour-over coffee, or reading and learning about something new. I've recently fallen in love with zen-ing out, learning how to meditate and do yoga, becoming a minimalist, and how to properly shoot a DSLR. </p>
 			
 			<?php include 'includes/footer.php'; ?></div>
 		
